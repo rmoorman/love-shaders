86c4ce9af15f7088951a949166a627907179af7a
Amadiro <jwringstad@gmail.com>
2014-11-02 02:18:52 +0100

    added basic chromatic abberation

---
+++ chromatic-abberation/main.lua
@@ -0,0 +1,23 @@
+backgroundFile = "background.png"
+shaderFile = "material.fsh"
+background = nil
+
+function loadShader()
+   shader = love.graphics.newShader(shaderFile)
+   print(shader:getWarnings())
+end
+
+function love.load(args)
+   background = love.graphics.newImage(backgroundFile)
+   love.window.setMode(background:getWidth(), background:getHeight(), {fullscreen = false})
+   loadShader()
+end
+
+
+function love.draw()
+   love.graphics.setShader(shader)
+   strength = math.sin(love.timer.getTime()*2)
+   shader:send("abberationVector", {strength*math.sin(love.timer.getTime()*7)/120, strength*math.cos(love.timer.getTime()*7)/120})
+   love.graphics.draw(background)
+   love.graphics.setShader()
+end
